9b01bc46bc204bffa0d4cd1a6406e448fbc24e2e
李家朋 <accounts_5c921fd1bb0faf00185f2dd2@mail.teambition.com>
2021-01-20 18:59:06 +0800

    feat: 上传头像

--- src/api/user.js
+++ src/api/user.js
@@ -1,31 +1,9 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: "/api/crm/v1/auth/login",
-    method: "post",
-    data
-  });
-}
-
-export function logout() {
-  return Promise.resolve();
-}
-
-// 获取验证码
-export function getCode(data) {
-  return request({
-    url: "/api/halo-dashboard/v1/phone/code",
-    method: "post",
-    data
-  });
-}
-
-// 更改用户tab栏
-export function updateTab(data) {
-  return request({
-    url: "/api/halo-dashboard/v1/user/menu",
-    method: "put",
-    data
-  });
+    url: '/api/crm/v1/auth/login',
+    method: 'post',
+    data,
+  })
 }
--- src/store/user.js
+++ src/store/user.js
@@ -1,43 +1,43 @@
-import * as Api from "@/api/user";
-import authority from "@/utils/authority";
+// import * as Api from '@/api/user'
+import authority from '@/utils/authority'
 
 const state = {
-  user: authority.get() || {}
-};
+  user: authority.get() || {},
+}
 
 const getters = {
   isLogin(state) {
-    return !!state.token;
-  }
-};
+    return !!state.token
+  },
+}
 
 const mutations = {
   SET_USER: (state, payload = {}) => {
-    Object.assign(state, payload);
-  }
-};
+    Object.assign(state, payload)
+  },
+}
 
 const actions = {
   // 登录
-  async login({ commit }, { phone = "", password = "" }) {
-    const data = await Api.login({
-      phone: phone.trim(),
-      password: password.trim()
-    });
-    data.is_admin = 1;
-    commit("SET_USER", { user: data });
+  async login({ commit }, { phone = '', password = '' }) {
+    // const data = await Api.login({
+    //   phone: phone.trim(),
+    //   password: password.trim()
+    // });
+    // data.is_admin = 1;
+    commit('SET_USER', { user: { token: 1, phone, password } })
   },
   // 登出
   async logout({ commit }) {
-    await Api.logout();
-    commit("SET_USER", { user: {} });
-    window.location.reload();
-  }
-};
+    authority.clear()
+    commit('SET_USER', { user: {} })
+    window.location.reload()
+  },
+}
 
 export default {
   state,
   getters,
   mutations,
-  actions
-};
+  actions,
+}
