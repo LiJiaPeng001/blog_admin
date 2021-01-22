af41c953d66980be173c6f7a4bff49eea5021062
李家朋 <accounts_5c921fd1bb0faf00185f2dd2@mail.teambition.com>
2021-01-22 17:37:18 +0800

    feat: 修改路径

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from 'vuex'
 
-const files = require.context('.', false, /\.js$/)
+const files = require.context('./', true, /\.js$/)
+
 const modules = {}
 
 files.keys().forEach((key) => {
   if (key === './index.js') return
-  modules[key.replace(/(\.\/|\.js)/g, '')] = files(key).default
+  let name = key.replace(/^\.\/modules\//g, '').replace(/\.js$/g, '')
+  modules[name] = files(key).default
 })
 
 export default createStore({
